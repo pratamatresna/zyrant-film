3b8a16fe82db85859dc2b08295f2cb7364037dee
pratamatresna <pratama.tresna08@gmail.com>
2024-12-19 13:35:09 +0700

    Pesan deskriptif mengenai perubahan

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default {
-  build: {
-    outDir: "dist", // Output folder after build
-  },
-};
+export default defineConfig({
+  plugins: [react()],
+  base: "/repository-name/",
+});
